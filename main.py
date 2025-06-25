4994c93e053e059b8c354ffb795f93903af1e75f
theorvt <theo.rivet14042003@gmail.com>
2025-06-26 01:29:41 +1200

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ if "results" not in st.session_state or st.session_state.get("last_params") != p
         
         
     # Calcul du spectre de Fourrier
-    T0_list = np.logspace(np.log10(0.01), np.log10(10.0), 100)
+    T0_list = np.logspace(np.log10(0.01), np.log10(30.0), 200)
     
     Sd, Sv, Sa = [], [], []
     
