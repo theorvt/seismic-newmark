ec51f7f200773e372350d3f9280513ba524b6406
theorvt <theo.rivet14042003@gmail.com>
2025-07-15 14:19:21 +1200

    Update main.py

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ if "results" not in st.session_state or st.session_state.get("last_params") != p
     # Modèle linéaire avec friction
     for i in range(n - 1): 
         # Friction régulière (approximation continue)
-        friction = mu * N_force * np.tanh(v_friction[i] / v_eps) 
+        friction = mu * N * (2 / np.pi) * np.arctan(v / eps)
 
         # Force totale (avec frottement)
         F_friction[i+1] = F[i+1] - friction
