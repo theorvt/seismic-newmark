7bfdfa79796a0fdba50177877e0b6645627f2951
theorvt <theo.rivet14042003@gmail.com>
2025-08-26 15:23:12 +1200

    d

--- main.py
+++ main.py
@@ -547,6 +547,9 @@ if "results" not in st.session_state or st.session_state.get("last_params") != p
         acc_interp_etage = []
         accel_etage = []
     
+        st.write("Shape acc_data_etage:", np.shape(acc_data_etage))
+        st.write("Length time_data_etage:", len(time_data_etage))
+    
         for i in range(len(acc_data_etage)):
             f_interp = interp1d(time_data_etage, acc_data_etage[i], kind='linear', fill_value='extrapolate')
             acc_interp_etage.append(f_interp)   # stocke la fonction
