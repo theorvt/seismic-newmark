76d732aa937fcfa77a57a7067b3dc817f7134eb4
theorvt <theo.rivet14042003@gmail.com>
2025-08-27 13:13:53 +1200

    Update main.py

--- main.py
+++ main.py
@@ -860,12 +860,20 @@ with col1:
     st.pyplot(fig)
  
     
+
+    
 # Affichage du graphiques pour les étages
 with col2:
     st.markdown("Te Puni building floor reaction")
 
     fig, ax = plt.subplots()
-    for j in range(12):  
+    for j in range(12):
+        b = len(Sa_etage[j])
+        c = len(T0_list_etage)
+        
+        f_interp_etage = interp1d(np.linspace(0,1,b), Sa_etage[j], kind='linear')
+        Sa_interp = f_interp_etage(np.linspace(0,1, c))
+        
         ax.plot(T0_list_etage, Sa_etage[j], label=f"Floor {j+1}")
     ax.set_xlabel("Period (s)")
     ax.set_ylabel("Peak Acceleration")
